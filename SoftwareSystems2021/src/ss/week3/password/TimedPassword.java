d1c65400d08d4e123eac899a6bdcb10aa26c81c6
Jan Willem Nijenhuis <janwillem.nijenhuis@nedap.com>
2021-12-02 16:01:59 +0100

    added 3.20

--- SoftwareSystems2021/src/ss/week3/password/TimedPassword.java
+++ SoftwareSystems2021/src/ss/week3/password/TimedPassword.java
@@ -13,55 +13,64 @@ public class TimedPassword extends Password {
     public static void main(String[] args) throws InterruptedException {
         TimedPassword timedPass = new TimedPassword();
         timedPass.setWord(timedPass.getInitPass(), "newpass");
-        TimeUnit.SECONDS.sleep(1);
         System.out.println(timedPass.testWord("newpass"));
-        System.out.println(timedPass.testWord("wrongpass"));
-        TimeUnit.SECONDS.sleep(9);
-        System.out.println("test");
+        TimeUnit.SECONDS.sleep(10);
+        timedPass.testWord("newpass");
+
 
     }
 
-    public TimedPassword() throws InterruptedException {
+    public TimedPassword() {
         super(new BasicChecker());
         this.validTime = defaultTime;
         this.startTime = System.currentTimeMillis();
-        this.checkTime();
     }
 
-    public TimedPassword(long validTime) throws InterruptedException {
+    public TimedPassword(long validTime) {
         super(new BasicChecker());
         this.validTime = validTime;
         this.startTime = System.currentTimeMillis();
-        this.checkTime();
     }
 
 
-    public void checkTime() throws InterruptedException {
-        while(true) {
-            endTime = System.currentTimeMillis();
-            if (Math.abs(this.endTime - this.startTime) > this.validTime) {
-                isExpired = true;
-                System.err.println("The password has expired.");
-                System.exit(0);
-            }
+    public void checkTime() {
+        endTime = System.currentTimeMillis();
+        if (Math.abs(this.endTime - this.startTime) > this.validTime) {
+            isExpired = true;
         }
     }
 
-    public void returnError(boolean isExpired) {
-        if(isExpired) {
-
-            System.exit(0);
+    public void printError() {
+        if(this.isExpired) {
+            System.err.println("The password has expired.");
         }
     }
 
     @Override
     public boolean setWord(String oldpass, String newpass) {
-        if(super.setWord(oldpass, newpass)) {
-            this.startTime = System.currentTimeMillis();
-            return true;
-        }
-        else {
+        this.checkTime();
+        if(isExpired) {
+            printError();
             return false;
         }
+        return super.setWord(oldpass, newpass);
+    }
+
+    public boolean acceptable(String suggestion) {
+        this.checkTime();
+        if(isExpired) {
+            printError();
+            return false;
+        }
+        return super.acceptable(suggestion);
+    }
+
+    public boolean testWord(String test) {
+        this.checkTime();
+        if(isExpired) {
+            printError();
+            return false;
+        }
+        return super.testWord(test);
     }
 }
