f0293b098de26179f7ca193b0e45eb1023074493
janwillem.nijenhuis <j.w.nijenhuis@student.utwente.nl>
2021-12-08 11:26:36 +0100

    added 4.9

--- SoftwareSystems2021/src/ss/week4/tictactoe/TicTacToe.java
+++ SoftwareSystems2021/src/ss/week4/tictactoe/TicTacToe.java
@@ -1,10 +1,12 @@
 package ss.week4.tictactoe;
 
+import org.w3c.dom.Text;
+import ss.utils.TextIO;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        String name1 = args[0];
-        String name2 = args[1];
-        Player p1 = new Player(name1, Mark.XX) {
+        String[] names = checkArgs(args);
+        Player p1 = new Player(names[0], Mark.XX) {
             @Override
             public int determineMove(Board board) {
                 int move;
@@ -14,7 +16,7 @@ public class TicTacToe {
                 return move;
             }
         };
-        Player p2 = new Player(name2, Mark.OO) {
+        Player p2 = new Player(names[1], Mark.OO) {
             @Override
             public int determineMove(Board board) {
                 int move;
@@ -27,5 +29,23 @@ public class TicTacToe {
 
         Game game = new Game(p1, p2);
         game.start();
+    }
+
+    public static String[] checkArgs(String[] args) {
+        String[] names = new String[2];
+        if (args.length >= 2) {
+            names[0] = args[0];
+            names[1] = args[1];
+        } else if (args.length == 1) {
+            names[0] = args[0];
+            System.out.println("Please input the name of the second player: ");
+            names[1] = TextIO.getlnString();
+        } else {
+            System.out.println("Please input the name of the first player: ");
+            names[0] = TextIO.getlnString();
+            System.out.println("Please input the name of the second player: ");
+            names[1] = TextIO.getlnString();
+        }
+        return names;
     }
 }
