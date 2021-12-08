4d5681cc990330c8b230b2ca177a13cbc928b1b9
janwillem.nijenhuis <j.w.nijenhuis@student.utwente.nl>
2021-12-08 16:07:56 +0100

    added until 4.15

--- SoftwareSystems2021/src/ss/week4/MapUtil.java
+++ SoftwareSystems2021/src/ss/week4/MapUtil.java
@@ -27,6 +27,9 @@ public class MapUtil {
         return true;
     }
 
+    /*@
+        ensures map.isEmpty() ==> \result == true;
+    */
     public static <K, V> boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
         if (!map.isEmpty()) {
             for (V v : range) {
@@ -38,7 +41,9 @@ public class MapUtil {
         return true;
     }
 
-    // map should be injective
+    /*@
+        ensures map.keySet().isEmpty() ==> \result.isEmpty();
+    */
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
         Map<V, Set<K>> inverse = new HashMap<>();
         Set<V> values = new HashSet<V>(map.values());
@@ -57,7 +62,11 @@ public class MapUtil {
         return inverse;
     }
 
-    // map should be injective and surjective
+    /*@
+        requires map.keySet().size() == map.values().size();
+        ensures inverseBijection(map).keySet().size() == inverseBijection(map).values().size();
+        ensures map == inverse(inverse(inverseBijection(map)));
+    */
     public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
         Map<V, K> bijection = new HashMap<>();
         Set<V> values = new HashSet<V>(map.values());
@@ -73,15 +82,40 @@ public class MapUtil {
         }
         return bijection;
     }
-	
+
+    /*@
+          ensures f.values().isEmpty() && g.keySet().isEmpty() ==> \result == true;
+          ensures !f.values().isEmpty() && g.keySet().isEmpty() ==> \result == true;
+    */
     public static <K, V, W> boolean compatible(Map<K, V> f, Map<V, W> g) {
-         // TODO: implement, see exercise P-4.13
-         return false;
+        Set<V> values = new HashSet<>(f.values());
+        Set<V> keys = new HashSet<>(g.keySet());
+        if (!values.isEmpty() && !keys.isEmpty()) {
+            for (V v : values) {
+                if (!keys.contains(v)) {
+                    return false;
+                }
+            }
+        } else if (!values.isEmpty() && keys.isEmpty()) {
+            return false;
+        }
+        return true;
     }
-	
+
+    /*@
+        requires isOneOnOne(f) && isOneOnOne(inverse(g));
+        ensures f.keySet() == \result.keySet();
+        ensures g.values() == \result.values();
+     */
     public static <K, V, W> Map<K, W> compose(Map<K, V> f, Map<V, W> g) {
-         // TODO: implement, see exercise P-4.13
-         return null;
+        Map<K, W> result = new HashMap<>();
+        Set<K> keys = new HashSet<>(f.keySet());
+        Set<W> values = new HashSet<>(g.values());
+        for (K k: keys) {
+            W w = g.get(f.get(k));
+            result.put(k, w);
+        }
+        return result;
     }
 	
 }
