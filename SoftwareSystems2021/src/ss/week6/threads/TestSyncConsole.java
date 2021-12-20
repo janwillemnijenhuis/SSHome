84547207968c04d3c798e89377e09fd7803906b1
janwillem.nijenhuis <j.w.nijenhuis@student.utwente.nl>
2021-12-20 14:47:55 +0100

    added until 6.15

--- SoftwareSystems2021/src/ss/week6/threads/TestSyncConsole.java
+++ SoftwareSystems2021/src/ss/week6/threads/TestSyncConsole.java
@@ -14,20 +14,22 @@ public class TestSyncConsole implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        TestSyncConsole t = new TestSyncConsole();
         Thread t1 = new Thread(new TestSyncConsole(), "Thread A");
         Thread t2 = new Thread(new TestSyncConsole(), "Thread B");
         t1.start();
         t1.join();
         t2.start();
-        /// Excercise 6.11 ///
+        /// Exercise 6.11 ///
         // they are not processed in the same order, first B1, A1, A2, B2
-        // Exercise 6.12 there is no difference. synchronized is used s.t. only a single thread can access a method at the same time.
-        // both can access run() at the same time. then only the first can access sum(), but when he does he goes into readint().
-        // then the second thread can access sum() now as the first thread has moved into readint(). Now first we ask for number 1 of A (or B)
-        // and then for number 1 of the second, since readint() is synchronized. But the order in which number 2 is processed can be different or the same,
-        // depending on which thread is done last, as this thread is the first to go back to sum(), since this is synchronized. Hence you will get the order
-        // A1, B1, B2, A2 and then both are printed.
-        // using join thread B has to wait for A
+        /// Exercise 6.12 ///
+        // there is no difference. synchronized is used s.t. only a single thread can access a method at the same time.
+        // but we create two new instances of testsyncconsole. therefore they can both access their own methods
+        // if we would create one instance it would work, or we can use join
+        /// Exercise 6.14 ///
+        // 1. What does it mean for a lock to be reentrant? the thread holding the lock can re-enter into the lock and hence create a queue before it is unlocked. Every time the thread unlocks the hold count is decremented by one
+        // 2. Is this behaviour different from the synchronized statement? yes if the thread occupying a method with the syncronized statement leaves the method any thread can take the lock
+        // 3. What would be advantages of using a ReentrantLock? you have more flexibility in when you want a thread to go first
+        // 4. And what would be disadvantages? you would have to unlock each lock and cannot forget this
     }
-
 }
